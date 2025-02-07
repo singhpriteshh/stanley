81e53610b964ccaabbbc6d2d02f672f5dd58faae
mehtachandrashekhar <mehtachandrashekhar84@gmail.com>
2025-02-07 12:15:42 +0530

    Added Containerization

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ mongoose
     .then(() => console.log("MongoDB connected"))
     .catch((error) => console.log("error"));
     
-
+        
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -33,7 +33,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/api/auth', authRouter);
+app.use('/api/auth', authRouter);       
 
 
 
